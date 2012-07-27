629e9c62f1e19f5525fdfe2d806aa96a1a200637
dggomes <dg.gomes@gmail.com>
2012-07-27 14:28:52 +0100

    iphone optimized

---
+++ busChoice/trunk/view.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Which Bus?</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script type="text/JavaScript" src="toggle.js"></script> 
+<meta name="viewport" content="width=device-width, user-scalable=no" />
+
+</head>
+<body>
+<?php include("transport.php"); ?>
+
+<!--
+
+-->
+
+<ul id="menu" class="roundedcorner">
+		<li><a id="displayBus17" href="javascript:toggleBus17();">Bus 17</a></li>
+		<li><a id="displayBus153" href="javascript:toggleBus153();">Bus 153</a></li>
+		<li><a id="displayBus91" href="javascript:toggleBus91();">Bus 91</a></li>
+		<li><a id="displayTube" href="javascript:toggleTube();">Predic</a></li>
+		<li><a id="displayTfl" href="javascript:toggleTFL();">TFL</a></li>
+</ul>
+
+
+<div id="bestChoice" class="roundedcorner">
+
+		<h2><?php echo $bestChoice; ?></h2>
+
+</div>
+
+<div id="bus17" class="data roundedcorner" style="display: none">
+
+		<h1>Bus 17</h1>
+		<h2>Walk BusStop: <?php echo ($walkTimeBus17/60); ?> min</h2>
+		<h2>Arrival at Bus Stop: <?php echo $arrivalatBusStopForBus17Epoch; ?></h2>
+		<h2>Arrival at Bus Stop: <?php echo date("H:i:s", $arrivalatBusStopForBus17Epoch); ?></h2>
+		<h2>Route: <?php echo ($bus17Route/60); ?> min</h2>
+		<h2>Walk Office: <?php echo ($walkDistanceBus17/60); ?> min</h2>
+		<h2>Arrival Office: <?php echo date("H:i:s", $bus17OfficeArrivalTime); ?></h2>
+
+</div>
+
+<div id="bus153" class="data roundedcorner" style="display: none">
+
+		<h1>Bus 153</h1>
+		<h2>Walk BusStop: <?php echo ($walkTimeBus153/60); ?> min</h2>
+		<h2>Arrival at Bus Stop: <?php echo $arrivalatBusStopForBus153Epoch; ?></h2>
+		<h2>Arrival at Bus Stop: <?php echo date("H:i:s", $arrivalatBusStopForBus153Epoch); ?></h2>
+		<h2>Route: <?php echo ($bus153Route/60); ?> min</h2>
+		<h2>Walk Office: <?php echo ($walkDistanceBus153/60); ?> min</h2>
+		<h2>Arrival Office: <?php echo date("H:i:s", $bus153OfficeArrivalTime); ?></h2>
+
+</div>
+
+<div id="bus91" class="data roundedcorner" style="display: none">
+
+		<h1>Bus 91</h1>
+		<h2>Walk BusStop: <?php echo ($walkTimeBus91/60); ?> min</h2>
+		<h2>Arrival at Bus Stop: <?php echo $arrivalatBusStopForBus91Epoch; ?></h2>
+		<h2>Arrival at Bus Stop: <?php echo date("H:i:s", $arrivalatBusStopForBus91Epoch); ?></h2>
+		<h2>Route: <?php echo ($bus91Route/60); ?> min</h2>
+		<h2>Walk Office: <?php echo ($walkDistanceBus91/60); ?> min</h2>
+		<h2>Arrival Office: <?php echo date("H:i:s", $bus91OfficeArrivalTime); ?></h2>
+
+</div>
+
+<div id="tfl" class="data roundedcorner" style="display: none">
+
+		<h1>TFL</h1>
+		<h2>BUS 17</h2>
+		<h2>1st Prediction: <?php echo date("H:i:s", $predictions17[0][1]); ?> || <?php if ($prediction17_valid1==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>2nd Prediction: <?php echo date("H:i:s", $predictions17[0][2]); ?> || <?php if ($prediction17_valid2==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>3rd Prediction: <?php echo date("H:i:s", $predictions17[0][3]); ?> || <?php if ($prediction17_valid3==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>4th Prediction: <?php echo date("H:i:s", $predictions17[0][4]); ?> || <?php if ($prediction17_valid4==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+
+		<h2>BUS 153</h2>
+		<h2>1st Prediction: <?php echo date("H:i:s", $predictions153[0][1]); ?> || <?php if ($prediction153_valid1==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>2nd Prediction: <?php echo date("H:i:s", $predictions153[0][2]); ?> || <?php if ($prediction153_valid2==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>3rd Prediction: <?php echo date("H:i:s", $predictions153[0][3]); ?> || <?php if ($prediction153_valid3==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>4th Prediction: <?php echo date("H:i:s", $predictions153[0][4]); ?> || <?php if ($prediction153_valid4==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+
+		<h2>BUS 91</h2>
+		<h2>1st Prediction: <?php echo date("H:i:s", $predictions91[0][1]); ?> || <?php if ($prediction91_valid1==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>2nd Prediction: <?php echo date("H:i:s", $predictions91[0][2]); ?> || <?php if ($prediction91_valid2==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>3rd Prediction: <?php echo date("H:i:s", $predictions91[0][3]); ?> || <?php if ($prediction91_valid3==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+		<h2>4th Prediction: <?php echo date("H:i:s", $predictions91[0][4]); ?> || <?php if ($prediction91_valid4==0) {echo INVALID;} else {echo VALID;}; ?></h2>
+
+</div>
+
+<div id="tube" class="data roundedcorner" style="display: none">
+
+		<h1>Conclusion</h1>
+		<h2>Bus 17: <?php echo date("H:i:s", $bus17EarliestArrivalTime); ?> || <?php echo date("H:i:s", $bus17OfficeArrivalTime); ?> || <?php echo $routeMin17; ?> min</h2>
+		<h2>Bus 153: <?php echo date("H:i:s", $bus153EarliestArrivalTime); ?> || <?php echo date("H:i:s", $bus153OfficeArrivalTime); ?> || <?php echo $routeMin153; ?> min</h2>
+		<h2>Bus 91: <?php echo date("H:i:s", $bus91EarliestArrivalTime); ?> || <?php echo date("H:i:s", $bus91OfficeArrivalTime); ?> || <?php echo $routeMin91; ?> min</h2>
+		<h2>Tube: <?php echo date("H:i:s", $tubeOfficeArrivalTime); ?> || <?php echo $routeMinTube; ?> min</h2>
+
+</div>
+
+<div id="mainCopy" class="roundedcorner">
+
+<h1><?php echo $bestChoiceCopy; ?></h1> 
+<h1><?php echo $walkAndWaitCopy; ?></h1>
+<h1><?php echo $journeyTimeCopy; ?></h1>
+
+</div>
+
+</body>
+</html>
